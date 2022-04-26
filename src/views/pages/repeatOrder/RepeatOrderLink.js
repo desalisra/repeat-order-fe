2da1a0bd6e277275b3d9417f851f632cb491d6d8
desalisra <desalisra48@gmail.com>
2022-04-26 09:50:24 +0700

    integrasi backend go

--- src/views/pages/repeatOrder/RepeatOrderLink.js
+++ src/views/pages/repeatOrder/RepeatOrderLink.js
@@ -1,17 +1,12 @@
 // Link dummy for testing using JSON - Server
 
 // base url
-const base_url = "http://localhost:3004";
-const base_url_go = "http://localhost:8080/reqorder/ro";
+const base_url = "http://localhost:8080/reqorder/ro";
 
 // Endpoint
-const get_products = base_url + "/products?_sort=procode&_order=asc";
-
-// const get_order_numb = base_url + "/th_reqprod";
-const get_order_numb = base_url_go + "/orders";
-//const get_request_order = base_url + "/th_reqprod?_embed=td_reqprod";
-const get_request_order = base_url_go + "/th_reqprod?_embed=td_reqprod";
+const get_products = base_url + "/procod";
+const get_request = base_url + "/orders";
 
 
-export { get_products, get_order_numb, get_request_order };
+export { get_products, get_request};
 
